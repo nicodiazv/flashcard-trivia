956330571235e18ec8dd9400e7de6b6126df101c
Nicolas Diaz Velar <e.diazvelar@gmail.com>
2020-06-06 21:05:50 -0300

    select categorias dinamico

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import FlashcardList from "./FlashcardList";
 import axios from "axios";
 import "./App.css";
 function App() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+  const [flashcards, setFlashcards] = useState([]);
+const [categories, setCategories] = useState([])
+  const categoryEl = useRef();
+  const amountEl = useRef();
 
   useEffect(() => {
-    axios
-      .get(
-        "https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple"
-      )
-      .then((res) => {
-        setFlashcards(
-          res.data.results.map((questionItem, index) => {
-            const answer = decodeString(questionItem.correct_answer);
-            const options = [
-              ...questionItem.incorrect_answers.map((a) => decodeString(a)),
-              answer,
-            ];
-            return {
-              id: `${index}-${Date.now()}`,
-              question: decodeString(questionItem.question),
-              answer: answer,
-              options: options.sort(() => Math.random() - 0.5),
-            };
-          })
-        );
-
-        console.log(res.data);
-      });
+    axios.get("https://opentdb.com/api_category.php").then((res) => {
+      console.log(res.data);
+      setCategories(res.data.trivia_categories)
+    });
   }, []);
+
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       "https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple"
+  //     )
+  //     .then((res) => {
+  //       setFlashcards(
+  //         res.data.results.map((questionItem, index) => {
+  //           const answer = decodeString(questionItem.correct_answer);
+  //           const options = [
+  //             ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+  //             answer,
+  //           ];
+  //           return {
+  //             id: `${index}-${Date.now()}`,
+  //             question: decodeString(questionItem.question),
+  //             answer: answer,
+  //             options: options.sort(() => Math.random() - 0.5),
+  //           };
+  //         })
+  //       );
+
+  //       console.log(res.data);
+  //     });
+  // }, []);
 
   const decodeString = (str) => {
     const textArea = document.createElement("textarea");
@@ -37,25 +47,67 @@ function App() {
     return textArea.value;
   };
 
-  return(
-  <div className="container">
-    <FlashcardList flashcards={flashcards} />
-  </div>)
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+    .get(
+      "https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple",
+      {
+        params: {
+          amount: amountEl.current.value,
+          category: categoryEl.current.value
+        }
+      }
+    )
+    .then((res) => {
+      setFlashcards(
+        res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer);
+          const options = [
+            ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+            answer,
+          ];
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - 0.5),
+          };
+        })
+      );
+
+      console.log(res.data);
+    });
+  }
+
+
+  return (
+    <>
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map(category => {
+              return <option value={category.id} key={category.id}>{category.name}</option>
+            })}
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="amount">Number Of Questions</label>
+          <input type="number"  id="amount" min="1" step="1" defaultValue={10} ref={amountEl}/>
+          </div>
+
+          <div className="form-group">
+            <button className="btn">Generate</button>
+          </div>
+      </form>
+      <div className="container">
+        <FlashcardList flashcards={flashcards} />
+      </div>
+    </>
+  );
 }
 
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: "Burpees",
-    answer: "10",
-    options: ["2", "3", "4", "5"],
-  },
-  {
-    id: 2,
-    question: "Question 2?",
-    answer: "Answer",
-    options: ["Answer", "Answer1", "Answer2", "Answer3"],
-  },
-];
 
 export default App;
